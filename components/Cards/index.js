0a00054273eb8ca8e09351d282caf91d08cb5ccb
Tate Keller <tatek1993@gmail.com>
2020-01-17 12:55:50 -0600

    fixed photos on article tiles

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -18,28 +18,48 @@
 //
 // Create a card for each of the articles and add the card to the DOM.
 
+
+// <div class="card">
+//     <div class="headline">ES8: The Next Step in the Evolution of Javascript and What it Means For Your Projects</div>
+//     <div class="author">
+//         <img src="./assets/sir.jpg">
+//     </div>
+//     <span>SIR RUFF'N'STUFF</span>
+// </div>
+
+
 const entryPoint2 = document.querySelector('.cards-container');
 
 axios.get('https://lambda-times-backend.herokuapp.com/articles')
 .then(response => {
-    debugger;
+    // debugger;
+     const articlesArray = Object.values(response.data.articles);
+    articlesArray.forEach(array => {
+       
 
-    entryPoint2.append(CardCreator())
+        array.forEach(article => {
+            entryPoint2.append(CardCreator(article.headline, article.authorPhoto, article.authorName))    
+         })    
+    })
+    
 })
 
 function CardCreator(headline, photo, name){
     const card = document.createElement('div'),
           cardHeadline = document.createElement('div'),
           cardAuthor = document.createElement('div'),
+          imgContainer = document.createElement('div'),
           cardImg = document.createElement('img'),
           cardName = document.createElement('span');
           
     card.append(cardHeadline, cardAuthor, cardName);
-    cardAuthor.append(cardImg);
+    imgContainer.append(cardImg);
+    cardAuthor.append(imgContainer,cardName);
     
     card.classList.add('card');
     cardHeadline.classList.add('headline');
     cardAuthor.classList.add('author');
+    imgContainer.classList.add('img-container');
 
     cardHeadline.textContent = headline;
     cardImg.src = photo;
